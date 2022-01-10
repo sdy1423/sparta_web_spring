06aa92a980ed2282679e31f3a977a79754852966
deokyong <deokyong93@gmail.com>
2022-01-10 19:09:42 +0900

    메모 생성,조회,변경,삭제

--- week03/src/main/java/com/sparta/week03/Week03Application.java
+++ week03/src/main/java/com/sparta/week03/Week03Application.java
@@ -2,7 +2,9 @@ package com.sparta.week03;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing //생성시간, 수정시간을 자동으로 Update 해준다.
 @SpringBootApplication
 public class Week03Application {
 
--- week03/src/main/java/com/sparta/week03/domain/Timestamped.java
+++ week03/src/main/java/com/sparta/week03/domain/Timestamped.java
@@ -1,5 +1,6 @@
 package com.sparta.week03.domain;
 
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 
 @MappedSuperclass // Timestamped 클래스를 상속하는 녀석이 Entity를 자동으로 컬럼으로 인식합니다.
 @EntityListeners(AuditingEntityListener.class) // 생성/변경 시간을 자동으로 업데이트합니다.
+@Getter
 public abstract class Timestamped {
 //abstract는 다른데서 상속해야 만 쓸 수 있는 클래스다.
     @CreatedDate
